11ef3333e925e42c5e60bc9f615057e0815025f1
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2019-05-31 12:15:57 -0300

    Fixing Linux regexxing

--- Sources/Console/Regexp.swift
+++ Sources/Console/Regexp.swift
@@ -13,31 +13,54 @@ func regexpMatches(regex regexString: String, in string: String) throws -> [Rege
         return []
     }
     
+    let cStringBuffer = UnsafeBufferPointer(start: cString, count: cString.count)
+    guard let cStringPointer = cStringBuffer.baseAddress else {
+        return []
+    }
+    
+    var cPointerOffset = 0
+    
     let maxMatches = 1024
     
     var result: [RegexpMatch] = []
-    var matches: [regmatch_t] = [regmatch_t](repeating: regmatch_t(), count: maxMatches)
+    var matches: [regmatch_t] = [regmatch_t](repeating: regmatch_t(), count: 1)
     var regex = regex_t()
     
-    if regexString.withCString({ cString in regcomp(&regex, cString, Int32(strlen(cString))) }) != 0 {
+    if regexString.withCString({ cString in regcomp(&regex, cString, REG_EXTENDED) }) != 0 {
         throw RegexError.invalidRegexString
     }
     
-    #if os(Linux)
-    if regexec(&regex, cString, maxMatches, &matches, 0) == Int32(REG_NOMATCH.rawValue) {
-        return []
-    }
-    #else
-    if regexec(&regex, cString, maxMatches, &matches, 0) == REG_NOMATCH {
-        return []
-    }
-    #endif
-    
-    for match in matches where match.rm_so != -1 {
-        let start = string.index(string.startIndex, offsetBy: Int(match.rm_so))
-        let end = string.index(string.startIndex, offsetBy: Int(match.rm_eo))
+    for _ in 0..<maxMatches {
+        if cPointerOffset >= cStringBuffer.count {
+            break
+        }
         
-        result.append(RegexpMatch(range: start..<end))
+        #if os(Linux)
+        if regexec(&regex, cStringPointer + cPointerOffset, 1, &matches, 0) == Int32(REG_NOMATCH.rawValue) {
+            break
+        }
+        #else
+        if regexec(&regex, cStringPointer + cPointerOffset, 1, &matches, 0) == REG_NOMATCH {
+            break
+        }
+        #endif
+        
+        var matched: Bool = false
+        
+        for match in matches where match.rm_so != -1 {
+            let start = string.index(string.startIndex, offsetBy: cPointerOffset + Int(match.rm_so))
+            let end = string.index(string.startIndex, offsetBy: cPointerOffset + Int(match.rm_eo))
+            
+            cPointerOffset += Int(match.rm_eo)
+            
+            result.append(RegexpMatch(range: start..<end))
+            
+            matched = true
+        }
+        
+        if !matched {
+            break
+        }
     }
     
     return result
